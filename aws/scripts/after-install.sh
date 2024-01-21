ee6b5a45cf5e3bb8b96b118e4b980d142a90968c
EC2 Default User <ec2-user@ip-172-31-40-165.ec2.internal>
2024-01-21 06:00:34 +0000

    -h

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://jt6uede3erpc/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://devops-inthe-east-webappdeploymentbucket-jt6uede3erpc/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
