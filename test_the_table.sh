2636628a9d373a0343d348a8854e1d5b656c70c6
devops-inthe-east <111242687+devops-inthe-east@users.noreply.github.com>
2024-07-31 16:13:56 +0200

    Update test_the_table.sh

--- test_the_table.sh
+++ test_the_table.sh
@@ -41,6 +41,7 @@ echo "***********************************"
 namespace_count=$(get_count ns)
 node_count=$(get_count nodes)
 kube_system_pod_count=$(kubectl get po -n kube-system | wc -l)
+deamon_set_count=$(kubectl get daemonset -A | wc -l )
 pod_count=$(get_count po)
 app_deployment_count=$(kubectl get deploy -A | grep -E '22e933a8-3406-4e81-9382-c5d384ca510d|31674166-4673-4755-999e-e5bd9a9df150|4d1953c8-cdd1-40c5-be37-d9d6ce6df0b9|5a0aca15-8b6c-487b-9a9d-9e92102165a1|5a0aca15-8b6c-487b-9a9d-9e92102165a|6bb35783-b673-4bc2-adfa-ed083265537b|b2f05235-ba6b-47a5-9252-3a73f6ee83a5|' | wc -l)
 app_pod_count=$(kubectl get pods -A | grep -E '22e933a8-3406-4e81-9382-c5d384ca510d|31674166-4673-4755-999e-e5bd9a9df150|4d1953c8-cdd1-40c5-be37-d9d6ce6df0b9|5a0aca15-8b6c-487b-9a9d-9e92102165a1|5a0aca15-8b6c-487b-9a9d-9e92102165a|6bb35783-b673-4bc2-adfa-ed083265537b|b2f05235-ba6b-47a5-9252-3a73f6ee83a5' | wc -l)
@@ -50,11 +51,11 @@ ingress_count=$(kubectl get ing -A | grep nginx | wc -l)
 echo -e "\n===== Summary ====="
 print_color "$YELLOW" "Component\tCount"
 print_color "$YELLOW" "---------\t-----"
-print_color "$CYAN" "\tNamespaces:\t$namespace_count"
-print_color "$CYAN" "\tNodes:\t\t$node_count"
-print_color "$CYAN" "\tKube-System-Pods:\t\t$kube_system_pod_count"
-print_color "$CYAN" "\tPods:\t\t$pod_count"
-print_color "$CYAN" "\tDeployments:\t$app_deployment_count"
-print_color "$CYAN" "\tApp Pods:\t$app_pod_count"
-print_color "$CYAN" "\tIngress:\t\t$ingress_count"
+print_color "$CYAN" "Namespaces:\t$namespace_count"
+print_color "$CYAN" "Nodes:\t\t$node_count"
+print_color "$CYAN" "Kube-System-Pods:\t$kube_system_pod_count"
+print_color "$CYAN" "Pods:\t\t$pod_count"
+print_color "$CYAN" "Deployments:\t$app_deployment_count"
+print_color "$CYAN" "App Pods:\t$app_pod_count"
+print_color "$CYAN" "Ingress:\t\t$ingress_count"
 echo ""
