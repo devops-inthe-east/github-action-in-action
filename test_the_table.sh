be821b84e0d77739087ebdc81fc96fb3fafcf180
devops-inthe-east <111242687+devops-inthe-east@users.noreply.github.com>
2024-07-31 15:24:08 +0200

    Update test_the_table.sh

--- test_the_table.sh
+++ test_the_table.sh
@@ -45,7 +45,7 @@ Namespace_Count=$(get_count ns)
 Node_Count=$(get_count nodes)
 Kube_System_Pod_Count=$(kubectl get pods -A | grep -E 'kube-system' | wc -l)
 Pod_Count=$(get_count po)
-App_Deployment_count=$(kubectl get deploy -A | grep -E '22e933a8-3406-4e81-9382-c5d384ca510d|31674166-4673-4755-999e-e5bd9a9df150|4d1953c8-cdd1-40c5-be37-d9d6ce6df0b9|5a0aca15-8b6c-487b-9a9d-9e92102165a1|5a0aca15-8b6c-487b-9a9d-9e92102165a|6bb35783-b673-4bc2-adfa-ed083265537b|b2f05235-ba6b-47a5-9252-3a73f6ee83a5|' | wc -l)
+App_Deployment_Count=$(kubectl get deploy -A | grep -E '22e933a8-3406-4e81-9382-c5d384ca510d|31674166-4673-4755-999e-e5bd9a9df150|4d1953c8-cdd1-40c5-be37-d9d6ce6df0b9|5a0aca15-8b6c-487b-9a9d-9e92102165a1|5a0aca15-8b6c-487b-9a9d-9e92102165a|6bb35783-b673-4bc2-adfa-ed083265537b|b2f05235-ba6b-47a5-9252-3a73f6ee83a5|' | wc -l)
 App_Pod_Count=$(kubectl get pods -A | grep -E '22e933a8-3406-4e81-9382-c5d384ca510d|31674166-4673-4755-999e-e5bd9a9df150|4d1953c8-cdd1-40c5-be37-d9d6ce6df0b9|5a0aca15-8b6c-487b-9a9d-9e92102165a1|5a0aca15-8b6c-487b-9a9d-9e92102165a|6bb35783-b673-4bc2-adfa-ed083265537b|b2f05235-ba6b-47a5-9252-3a73f6ee83a5' | wc -l)
 Ingress_Count=$(kubectl get ing -A | grep nginx | wc -l)
 
@@ -54,10 +54,10 @@ echo -e "\n===== Summary ====="
 print_color "$YELLOW" "Component\tCount"
 print_color "$YELLOW" "---------\t-----"
 print_color "$CYAN" "Namespaces\t$Namespace_Count"
-print_color "$CYAN" "Nodes\t\t$node_count"
-print_color "$CYAN" "Pods\t\t$Kube_System_Pod"
+print_color "$CYAN" "Nodes\t\t$Node_Count"
+print_color "$CYAN" "Kube-System-Pod\t\t$Kube_System_Pod"
 print_color "$CYAN" "Pods\t\t$Pod_Count"
-print_color "$CYAN" "Deployments\t$Deployment_count"
+print_color "$CYAN" "Deployments\t$App_Deployment_Count"
 print_color "$CYAN" "App Pods\t$App_Pod_Count"
 print_color "$CYAN" "Ingress\t\t$Ingress_Count"
 echo ""
